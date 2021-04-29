9c48da8870e0ad3a3eeafc5ae44f929659d5f07e
Chase Manning <me@chasemanning.co.nz>
2021-04-29 13:12:27 +1000

    :fire: remove redundant networkId

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,97 +1,93 @@
-import React, { Component } from 'react'
-import Web3 from 'web3'
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import React, { Component } from "react";
+import Web3 from "web3";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-import Navbar from '../Navbar'
-import './App.css'
-import Farm from '../Farm/Farm';
+import Navbar from "../Navbar";
+import "./App.css";
+import Farm from "../Farm/Farm";
 
 class App extends Component {
-
   async componentWillMount() {
-    await this.loadWeb3()
-    await this.loadBlockchainData()
+    await this.loadWeb3();
+    await this.loadBlockchainData();
   }
 
   async loadBlockchainData() {
-    const web3 = window.web3
+    const web3 = window.web3;
 
-    const accounts = await web3.eth.getAccounts()
-    this.setState({ account: accounts[0] })
+    const accounts = await web3.eth.getAccounts();
+    this.setState({ account: accounts[0] });
 
-    const networkId = await web3.eth.net.getId()
-
-    this.setState({ loading: false })
+    this.setState({ loading: false });
   }
 
   async loadWeb3() {
     if (window.ethereum) {
-      window.web3 = new Web3(window.ethereum)
-      await window.ethereum.enable()
-    }
-    else if (window.web3) {
-      window.web3 = new Web3(window.web3.currentProvider)
-    }
-    else {
-      window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!')
+      window.web3 = new Web3(window.ethereum);
+      await window.ethereum.enable();
+    } else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider);
+    } else {
+      window.alert(
+        "Non-Ethereum browser detected. You should consider trying MetaMask!"
+      );
     }
   }
 
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      account: '0x0',
+      account: "0x0",
       stakingToken: {},
       rewradToken: {},
       tokenFarm: {},
-      stakingTokenBalance: '0',
-      rewardTokenBalance: '0',
-      stakingBalance: '0',
-      loading: true
-    }
+      stakingTokenBalance: "0",
+      rewardTokenBalance: "0",
+      stakingBalance: "0",
+      loading: true,
+    };
   }
 
   render() {
-    let content
-    if(this.state.loading) {
-      content = <p id="loader" className="text-center">Loading...</p>
+    let content;
+    if (this.state.loading) {
+      content = (
+        <p id="loader" className="text-center">
+          Loading...
+        </p>
+      );
     } else {
-      content = <p> based </p>
+      content = <p> based </p>;
     }
 
     return (
       <div>
         <Navbar account={this.state.account} />
         <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/farm">Farm</Link>
-            </li>
-          </ul>
-        </nav>
+          <div>
+            <nav>
+              <ul>
+                <li>
+                  <Link to="/">Home</Link>
+                </li>
+                <li>
+                  <Link to="/farm">Farm</Link>
+                </li>
+              </ul>
+            </nav>
 
-        {/* A <Switch> looks through its children <Route>s and
+            {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
-        <Switch>
-          <Route path="/farm">
-            <Farm />
-          </Route>
-          {/* <Route path="/">
+            <Switch>
+              <Route path="/farm">
+                <Farm />
+              </Route>
+              {/* <Route path="/">
             <Home />
           </Route> */}
-        </Switch>
-      </div>
-    </Router>
+            </Switch>
+          </div>
+        </Router>
       </div>
     );
   }
